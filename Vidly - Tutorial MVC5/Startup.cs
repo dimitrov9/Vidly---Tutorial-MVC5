075b97c6720279b738fe793ba6c963cfd10d10bb
dimitrov9 <dimitrov333@hotmail.com>
2018-07-11 15:54:30 +0200

    Add project files.

---
+++ Vidly - Tutorial MVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly___Tutorial_MVC5.Startup))]
+namespace Vidly___Tutorial_MVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
