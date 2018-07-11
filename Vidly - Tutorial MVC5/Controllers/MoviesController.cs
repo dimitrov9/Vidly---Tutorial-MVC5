c4be1625c27e1724fd15dff8ac96fa2e1461cb1b
dimitrov9 <dimitrov333@hotmail.com>
2018-07-11 17:40:45 +0200

    - Create Movie Model - Create Movie Controller and 1 Random Action to return a view - Random View for Movie to display the Movie Name - Changed the bootstrap theme and undated bootstrap to 4.1

---
+++ Vidly - Tutorial MVC5/Controllers/MoviesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Vidly___Tutorial_MVC5.Models;
+
+namespace Vidly___Tutorial_MVC5.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            return View(movie);
+        }
+    }
+}
---
+++ Vidly - Tutorial MVC5/Models/Movie.cs
@@ -0,0 +1,8 @@
+﻿namespace Vidly___Tutorial_MVC5.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
