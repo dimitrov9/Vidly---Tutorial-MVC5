da0649a66e8748b5aa997844dc0385fb8e895238
dimitrov9 <dimitrov333@hotmail.com>
2018-07-12 17:12:53 +0200

    Added data annotations to Name property in Customer Model making it Required and MaxLenght of 255, added migration and updated database

--- Vidly - Tutorial MVC5/Models/Customer.cs
+++ Vidly - Tutorial MVC5/Models/Customer.cs
@@ -1,10 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Vidly___Tutorial_MVC5.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+
+        [Required, StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
 
